d6559a5fb225f7b9dffae7753ae6733da6d01abb
gerhardadler <gerhardskartveit@gmail.com>
2023-06-02 11:32:18 +0200

    Now requires either csv recipients or normal

--- handle_mail_form.js
+++ handle_mail_form.js
@@ -2,6 +2,8 @@ async function handleMailForm() {
     const form = document.getElementById("mail_form");
     const formData = new FormData(form);
 
+    if (!validateMailForm(formData)) return;
+
     const response = await fetch("http://localhost:8000/send-email", {
         method: "POST",
         enctype: "multipart/form-data",
@@ -12,4 +14,12 @@ async function handleMailForm() {
     if (response.status !== 200) {
         alert(result.detail);
     } 
+}
+
+function validateMailForm(formData) {
+    if (formData.get("recipients") === "" && formData.get("csv_recipients").size === 0) {
+        alert("You must add recipients");
+        return false;
+    }
+    return true;
 }
